0e32d00525e69165951a4e207010bcc8ddbe7b2c
ruwix <grantpauker@gmail.com>
2018-11-07 10:02:39 -0800

    rename variables and update removing of encoder class

--- src/drive.py
+++ src/drive.py
@@ -14,21 +14,21 @@ class Drive(Subsystem):
        and a gyro.
     '''
 
-    def __init__(self, robot, leftMotorSlave, leftMotorMaster,  rightMotorSlave, rightMotorMaster):
+    def __init__(self, robot, left_motor_slave, left_motor_master,  right_motor_slave, right_motor_master):
         super().__init__()
 
         self.robot = robot
         self.timestamp = 0
-        self.lastTimestamp = 0
+        self.last_timestamp = 0
 
-        self.leftMotorSlave = leftMotorSlave
-        self.leftMotorMaster = leftMotorMaster
-        self.rightMotorSlave = rightMotorSlave
-        self.rightMotorMaster = rightMotorMaster
-        
-        self.rightMotorSlave.set(
+        self.left_motor_slave = left_motor_slave
+        self.left_motor_master = left_motor_master
+        self.right_motor_slave = right_motor_slave
+        self.right_motor_master = right_motor_master
+
+        self.right_motor_slave.set(
             ctre.ControlMode.Follower, constants.RIGHT_MOTOR_MASTER_ID)
-        self.leftMotorSlave.set(ctre.ControlMode.Follower,
+        self.left_motor_slave.set(ctre.ControlMode.Follower,
                                 constants.LEFT_MOTOR_MASTER_ID)
 
 
@@ -48,7 +48,39 @@ class Drive(Subsystem):
     def setPercentOutput(self, left_signal, right_signal):
         left_signal = min(max(left_signal, -1), 1)
         right_signal = min(max(right_signal, -1), 1)
-        self.leftMotorMaster.set(
+        self.left_motor_master.set(
             ctre.ControlMode.PercentOutput, left_signal)
-        self.rightMotorMaster.set(
+        self.right_motor_master.set(
             ctre.ControlMode.PercentOutput, right_signal)
+
+    def getDistanceTicksLeft(self):
+        return self.left_motor_master.getSelectedSensorPosition(0)
+
+    def getVelocityTicksLeft(self):
+        return self.left_motor_master.getSelectedSensorVeloicty(0)
+
+    def getDistanceTicksRight(self):
+        return self.right_motor_master.getSelectedSensorPosition(0)
+
+    def getVelocityTicksRight(self):
+        return self.right_motor_master.getSelectedSensorVeloicty(0)
+
+
+    def ticksToInchesLeft(self, ticks):
+        return (ticks/constants.DRIVE_ENCODER_TICKS_PER_REVOLUTION_LEFT)*constants.WHEEL_CIRCUMFERENCE
+
+    def ticksToInchesRight(self, ticks):
+        return (ticks/constants.DRIVE_ENCODER_TICKS_PER_REVOLUTION_RIGHT)*constants.WHEEL_CIRCUMFERENCE
+
+
+    def getDistanceInchesLeft(self):
+        return self.ticksToInchesLeft(self.getDistanceTicksLeft())
+
+    def getVelocityTicksInchesLeft(self):
+        return self.ticksToInchesLeft(self.getVelocityTicksLeft())
+
+    def getDistanceInchesRight(self):
+        return self.ticksToInchesRight(self.getDistanceTicksRight())
+
+    def getVelocityTicksInchesRight(self):
+        return self.ticksToInchesRight(self.getVelocityTicksRight())
