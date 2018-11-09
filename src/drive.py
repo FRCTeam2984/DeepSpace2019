1c3af988da24ef09d8909efa7ba9519aa78c9124
ruwix <grantpauker@gmail.com>
2018-11-08 16:51:12 -0800

    move motors an gyro to drive subsystem. make drive subsytem a singleton

--- src/drive.py
+++ src/drive.py
@@ -7,42 +7,42 @@ import constants
 import ctre
 import oi
 import tankdrive
+import singleton
 
-class Drive(Subsystem):
+class Drive(Subsystem, metaclass=singleton.Singleton):
     '''The DriveTrain subsystem incorporates the sensors and actuators attached to
        the robots chassis. These include four drive motors, a left and right encoder
        and a gyro.
     '''
 
-    def __init__(self, robot, left_motor_slave, left_motor_master,  right_motor_slave, right_motor_master):
+    def __init__(self):
         super().__init__()
 
-        self.robot = robot
         self.timestamp = 0
         self.last_timestamp = 0
 
-        self.left_motor_slave = left_motor_slave
-        self.left_motor_master = left_motor_master
-        self.right_motor_slave = right_motor_slave
-        self.right_motor_master = right_motor_master
+        self.left_motor_slave = ctre.WPI_TalonSRX(
+            constants.LEFT_MOTOR_SLAVE_ID)
+        self.left_motor_master = ctre.WPI_TalonSRX(
+            constants.LEFT_MOTOR_MASTER_ID)
+        self.right_motor_slave = ctre.WPI_TalonSRX(
+            constants.RIGHT_MOTOR_SLAVE_ID)
+        self.right_motor_master = ctre.WPI_TalonSRX(
+            constants.RIGHT_MOTOR_MASTER_ID)
 
         self.right_motor_slave.set(
             ctre.ControlMode.Follower, constants.RIGHT_MOTOR_MASTER_ID)
         self.left_motor_slave.set(ctre.ControlMode.Follower,
-                                constants.LEFT_MOTOR_MASTER_ID)
-
+                                  constants.LEFT_MOTOR_MASTER_ID)
+        self.gyro = wpilib.adxrs450_gyro.ADXRS450_Gyro(0)
 
     def initDefaultCommand(self):
-        '''When no other command is running let the operator drive around
-           using the PS3 joystick'''
-        self.setDefaultCommand(tankdrive.TankDrive(self.robot))
+        self.setDefaultCommand(tankdrive.TankDrive())
 
     def log(self):
-        '''The log method puts interesting information to the SmartDashboard.'''
         return
 
     def reset(self):
-        '''Reset the robots sensors to the zero states'''
         return
 
     def setPercentOutput(self, left_signal, right_signal):
@@ -65,13 +65,11 @@ class Drive(Subsystem):
     def getVelocityTicksRight(self):
         return self.right_motor_master.getSelectedSensorVeloicty(0)
 
-
     def ticksToInchesLeft(self, ticks):
         return (ticks/constants.DRIVE_ENCODER_TICKS_PER_REVOLUTION_LEFT)*constants.WHEEL_CIRCUMFERENCE
 
     def ticksToInchesRight(self, ticks):
         return (ticks/constants.DRIVE_ENCODER_TICKS_PER_REVOLUTION_RIGHT)*constants.WHEEL_CIRCUMFERENCE
-
 
     def getDistanceInchesLeft(self):
         return self.ticksToInchesLeft(self.getDistanceTicksLeft())
