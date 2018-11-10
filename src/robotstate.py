39e0635e05d73ea9a1d9ed1649ca9358471ef39c
ruwix <grantpauker@gmail.com>
2018-11-09 19:40:18 -0800

    fix odemtry and create and implment a pose class

--- src/robotstate.py
+++ src/robotstate.py
@@ -7,25 +7,23 @@ import constants
 from enum import Enum
 from subsystems import drive
 from utils import singleton
+from utils import pose
 
 class RobotState(metaclass=singleton.Singleton):
-
 
     def __init__(self):
         """Initilize the RobotState class."""
         super().__init__()
         self.drive = drive.Drive()
 
-        self.last_left_ecnoder_distance = 0
-        self.last_right_ecnoder_distance = 0
-
         self.timestamp = 0
         self.last_timestamp = 0
 
-        self.pos_x = 0
-        self.pos_y = 0
-        self.heading = 0
-        self.last_heading = 0
+        self.pose = pose.Pose()
+
+        self.last_left_ecnoder_distance = 0
+        self.last_right_ecnoder_distance = 0
+        self.last_angle = 0
 
     def outputToSmartDashboard(self):
         wpilib.SmartDashboard.putNumber(
@@ -33,9 +31,9 @@ class RobotState(metaclass=singleton.Singleton):
         wpilib.SmartDashboard.putNumber(
             "Right Encoder Inches", self.drive.getDistanceInchesRight())
         wpilib.SmartDashboard.putNumber("Gyro Angle", self.getAngle())
-        wpilib.SmartDashboard.putNumber("Pos X", self.getState()[0])
-        wpilib.SmartDashboard.putNumber("Pos Y", self.getState()[1])
-        wpilib.SmartDashboard.putNumber("Heading", self.getState()[2])
+        wpilib.SmartDashboard.putNumber("Pos X", self.pose.x)
+        wpilib.SmartDashboard.putNumber("Pos Y", self.pose.y)
+        wpilib.SmartDashboard.putNumber("Heading", self.pose.angle)
 
     def getDistance(self):
         """Use encoders to return the distance driven in inches."""
@@ -51,20 +49,20 @@ class RobotState(metaclass=singleton.Singleton):
 
     def getAngleDelta(self):
         """Use the gyroscope to return the angle change in radians."""
-        return self.heading-self.last_heading
+        return self.pose.angle-self.last_angle
 
     def updateState(self, timestamp):
         """Use odometry to update the robot state."""
         self.timestamp = timestamp
-        delta_time = self.timestamp-self.last_timestamp
-        self.heading += self.getAngleDelta()*delta_time
-        self.pos_x += self.getDistanceDelta()*math.cos(self.heading)*delta_time
-        self.pos_y += self.getDistanceDelta()*math.sin(self.heading)*delta_time
+        # delta_time = self.timestamp-self.last_timestamp
+        self.pose.angle = self.getAngle()
+        self.pose.x += self.getDistanceDelta()*math.cos(self.pose.angle)
+        self.pose.y += self.getDistanceDelta()*math.sin(self.pose.angle)
         self.last_left_ecnoder_distance = self.drive.getDistanceInchesLeft()
         self.last_right_ecnoder_distance = self.drive.getDistanceInchesRight()
-        self.last_heading = self.heading
+        self.last_angle = self.pose.angle
         self.last_timestamp = self.timestamp
 
     def getState(self):
-        """Return the robot position and heading."""
-        return [self.pos_x, self.pos_y, self.heading]
+        """Return the robot pose (position and orientation)."""
+        return pose
