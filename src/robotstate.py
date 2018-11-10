032f271ed2ed0e2bb9d8248bde6a5f079c9f9903
Cameron Reikes <cameronreikes@gmail.com>
2018-11-10 11:32:07 -0800

    Fix import statements

--- src/robotstate.py
+++ src/robotstate.py
@@ -1,6 +1,7 @@
 import math
-import wpilib as wpi
 import constants
+
+from wpilib import SmartDashboard as Dash
 from subsystems import drive
 from utils import singleton
 from utils import pose
@@ -23,14 +24,14 @@ class RobotState(metaclass=singleton.Singleton):
         self.last_angle = 0
 
     def outputToSmartDashboard(self):
-        wpi.SmartDashboard.putNumber(
+        Dash.putNumber(
             "Left Encoder Inches", self.drive.getDistanceInchesLeft())
-        wpi.SmartDashboard.putNumber(
+        Dash.putNumber(
             "Right Encoder Inches", self.drive.getDistanceInchesRight())
-        wpi.SmartDashboard.putNumber("Gyro Angle", self.getAngle())
-        wpi.SmartDashboard.putNumber("Pos X", self.pose.x)
-        wpi.SmartDashboard.putNumber("Pos Y", self.pose.y)
-        wpi.SmartDashboard.putNumber("Heading", self.pose.angle)
+        Dash.putNumber("Gyro Angle", self.getAngle())
+        Dash.putNumber("Pos X", self.pose.x)
+        Dash.putNumber("Pos Y", self.pose.y)
+        Dash.putNumber("Heading", self.pose.angle)
 
     def getDistance(self):
         """Use encoders to return the distance driven in inches."""
@@ -51,12 +52,16 @@ class RobotState(metaclass=singleton.Singleton):
     def updateState(self, timestamp):
         """Use odometry to update the robot state."""
         self.timestamp = timestamp
-        # delta_time = self.timestamp-self.last_timestamp
+        #delta_time = self.timestamp-self.last_timestamp
+        # update angle
         self.pose.angle = self.getAngle()
+        # update x and y positions
         self.pose.x += self.getDistanceDelta()*math.cos(self.pose.angle)
         self.pose.y += self.getDistanceDelta()*math.sin(self.pose.angle)
+        # update last distances for next periodic
         self.last_left_ecnoder_distance = self.drive.getDistanceInchesLeft()
         self.last_right_ecnoder_distance = self.drive.getDistanceInchesRight()
+        # update last angle and timestamp for next periodic
         self.last_angle = self.pose.angle
         self.last_timestamp = self.timestamp
 
