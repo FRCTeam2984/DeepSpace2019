cb09ea67349bd6864f5f5f49a703fe30653d1935
Cameron Reikes <cameronreikes@gmail.com>
2018-11-16 22:57:05 -0800

    Make easier to understand robot functionality

---
+++ src/commands/testgroup.py
@@ -0,0 +1,9 @@
+from wpilib.command import CommandGroup
+
+
+class TestCommandGroup(CommandGroup):
+    """Run when robot enters testing mode"""
+
+    def __init__(self):
+        super().__init__('Test Program')
+        # TODO add robot systems test
