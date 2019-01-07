7b73c9325b599203550db2377343252a58d03a5c
ruwix <grantpauker@gmail.com>
2019-01-07 15:56:05 -0800

    Create pursuit point class

--- src/autonomous/purepursuit.py
+++ src/autonomous/purepursuit.py
@@ -2,6 +2,7 @@ import math
 from constants import Constants
 from utils import vector2d
 from wpilib import SmartDashboard as Dash
+from autonomous import pursuitpoint
 
 
 class PurePursuit():
@@ -9,10 +10,8 @@ class PurePursuit():
 
     def __init__(self, path):
         self.path = path
-        self.points = self.path.getPoints()
-        self.curvatures = self.path.getCurvatures()
-        self.lookahead_dist = Constants.LOOKAHEAD_DIST
-        self.velocities = []
+        self.pursuit_points = [pursuitpoint.PursuitPoint(p, c) for p, c in zip(
+            self.path.getPoints(), self.path.getCurvatures())]
         self.last_lookahead_index = 0
         self.cur_curvature = 0
         self.target_velocities = vector2d.Vector2D()
@@ -21,48 +20,49 @@ class PurePursuit():
     def computeVelocities(self):
         """Compute the velocities along the path."""
         # Compute the velocities along the path using the curvature and Constants.CURVE_VELOCITY
-        for curvature in self.curvatures:
-            if abs(curvature) <= Constants.CURVATURE_THRESHOLD:
+        for ppoint in self.pursuit_points:
+            if abs(ppoint.curvature) <= Constants.CURVATURE_THRESHOLD:
                 velocity = Constants.MAX_VELOCITY
             else:
                 velocity = min(Constants.MAX_VELOCITY,
-                               Constants.CURVE_VELOCITY/curvature)
-            self.velocities.append(velocity)
+                               Constants.CURVE_VELOCITY/ppoint.curvature)
+            ppoint.velocity = velocity
+        print(self.pursuit_points[0].velocity)
         # Limit the acceleration of the velocities
-        for i in reversed(range(0, len(self.velocities)-1)):
-            distance = self.points[i].getDistance(self.points[i+1])
+        for i in reversed(range(0, len(self.pursuit_points)-1)):
+            distance = self.pursuit_points[i].point.getDistance(self.pursuit_points[i+1].point)
             new_velocity = math.sqrt(
-                self.velocities[i+1]**2 + (2 * Constants.MAX_ACCELERATION * distance))
-            new_velocity = min(self.velocities[i], new_velocity)
-            self.velocities[i] = new_velocity
+                self.pursuit_points[i+1].velocity**2 + (2 * Constants.MAX_ACCELERATION * distance))
+            new_velocity = min(self.pursuit_points[i].velocity, new_velocity)
+            self.pursuit_points[i].velocity = new_velocity
 
     def updateLookaheadPointIndex2(self, state):
         """Update the lookahead point given the current robot state.
            Uses the minimum distance point if the state is more than
-           self.lookahead_distance from all points, otherwise uses the
+           Constants.LOOKAHEAD_DIST from all points, otherwise uses the
            closes point to self.loohead_distance"""
-        # Compute point distances to state and differences from those distances to self.lookahead_distance
-        distances = [math.hypot(state.x - point.x,
-                                state.y - point.y) for point in self.points]
-        differences = [abs(d-self.lookahead_dist) for d in distances]
+        # Compute point distances to state and differences from those distances to Constants.LOOKAHEAD_DIST
+        distances = [math.hypot(state.x - ppoint.point.x,
+                                state.y - ppoint.point.y) for ppoint in self.pursuit_points]
+        differences = [abs(d-Constants.LOOKAHEAD_DIST) for d in distances]
         min_distance = min(distances)
         # Get new lookahead index
-        if min_distance <= self.lookahead_dist:
+        if min_distance <= Constants.LOOKAHEAD_DIST:
             self.last_lookahead_index = differences.index(min(differences))
         else:
             self.last_lookahead_index = distances.index(min_distance)
 
     def updateLookaheadPointIndex(self, state):
         """Loop over the points in the path to get the lookahead point given the current robot state."""
-        for i in range(self.last_lookahead_index, len(self.points)-1):
+        for i in range(self.last_lookahead_index, len(self.pursuit_points)-1):
             lookahead = self.computeLookaheadPoint(
-                self.points[i], self.points[i+1], state)
+                self.pursuit_points[i].point, self.pursuit_points[i+1].point, state)
             if lookahead != None:
                 self.last_lookahead_index = i
 
     def computeLookaheadPoint(self, start, end, state):
         """Compute the lookahead point given the current robot state.
-           Returns a point if the current state is self.lookahead_distance
+           Returns a point if the current state is Constants.LOOKAHEAD_DIST
            from between start and end, otherwise returns None."""
         # Algorithm for circle line segment intersection found here: https://stackoverflow.com/questions/1073336/circle-line-segment-collision-detection-algorithm/1084899#1084899
         segment_direction = end - start
@@ -70,7 +70,7 @@ class PurePursuit():
 
         a = segment_direction * segment_direction
         b = 2 * (center_to_start * segment_direction)
-        c = (center_to_start * center_to_start) - self.lookahead_dist ** 2
+        c = (center_to_start * center_to_start) - Constants.LOOKAHEAD_DIST ** 2
         discriminant = b**2 - (4 * a * c)
 
         if discriminant < 0:
@@ -87,19 +87,19 @@ class PurePursuit():
 
     def updateCurvature(self, state):
         """Update the curvature from the current lookahead point to the current robot position."""
-        lookahead = self.points[self.last_lookahead_index]
+        lookahead = self.pursuit_points[self.last_lookahead_index].point
         # Transform the lookahead and state.pos to get an aligned vector
         transform = lookahead - state.pos
         transform = transform.getRotated(-state.angle)
         # Use the transformed vector to calculate the curvature (derived from https://www.ri.cmu.edu/pub_files/pub3/coulter_r_craig_1992_1/coulter_r_craig_1992_1.pdf#page=12)
-        self.cur_curvature = (2 * transform.x) / self.lookahead_dist**2
+        self.cur_curvature = (2 * transform.x) / Constants.LOOKAHEAD_DIST**2
 
     def updateClosestPointIndex(self, state):
         """Update the index of the closest point to the current robot position."""
         index = self.closest_point_index
-        smallest_distance = self.points[index].getDistance(state)
-        for i in range(0, len(self.points)):
-            distance = self.points[i].getDistance(state)
+        smallest_distance = self.pursuit_points[index].point.getDistance(state)
+        for i in range(0, len(self.pursuit_points)):
+            distance = self.pursuit_points[i].point.getDistance(state)
             if smallest_distance > distance:
                 smallest_distance = distance
                 index = i
@@ -107,7 +107,7 @@ class PurePursuit():
 
     def updateTargetVelocities(self, state):
         """Update the target velocities of the left and right wheels."""
-        robot_velocity = self.velocities[self.closest_point_index]
+        robot_velocity = self.pursuit_points[self.closest_point_index].velocity
         # Use kinematics (http://robotsforroboticists.com/drive-kinematics/) and algebra to find wheel target velocties
         l_velocity = robot_velocity * \
             (2 + self.cur_curvature * Constants.TRACK_WIDTH) / \
@@ -128,8 +128,8 @@ class PurePursuit():
 
     def outputToSmartDashboard(self):
         """Output values to the smart dashboard."""
-        lookahead = self.points[self.last_lookahead_index]
-        closest = self.points[self.closest_point_index]
+        lookahead = self.pursuit_points[self.last_lookahead_index].point
+        closest = self.pursuit_points[self.closest_point_index].point
         Dash.putNumberArray("Lookahead Point", [lookahead.x, lookahead.y])
         Dash.putNumber("Curvature", self.cur_curvature)
         Dash.putNumberArray("Closes Point", [closest.x, closest.y])
@@ -143,4 +143,4 @@ class PurePursuit():
 
     def isDone(self):
         """Check if the path is done being followed."""
-        return (len(self.points) - self.closest_point_index) <= 1
+        return (len(self.pursuit_points) - self.closest_point_index) <= 1
