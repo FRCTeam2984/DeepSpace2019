e9f6fdfe94bef6c112eaaf7b7d0cd8a8f1431b43
ruwix <grantpauker@gmail.com>
2019-01-08 18:23:17 -0800

    Add deadzone

--- src/utils/joystick.py
+++ src/utils/joystick.py
@@ -1,23 +1,36 @@
 from wpilib import joystick
+from wpilib.buttons import joystickbutton
 
 
 class Joystick(joystick.Joystick):
-    """A wraper for the wpilib joystick library that allows you to easily modify x, y, and z axis values."""
+    """A wraper for the wpilib joystick library that allows you
+       to easily modify x, y, and z axis values."""
 
-    def __init__(self, port, x_modifier, y_modifier, z_modifier):
+    def __init__(self, port, x_modifier, y_modifier, z_modifier, deadzone=0.05):
         super().__init__(port)
         self.x_modifier = x_modifier
         self.y_modifier = y_modifier
         self.z_modifier = z_modifier
+        self.deadzone = deadzone
 
     def getX(self):
         """Get the modified X axis."""
-        return self.x_modifier * super().getX()
+        value = super().getX()
+        if abs(value) < self.deadzone:
+            return 0
+        return self.x_modifier * value
 
     def getY(self):
         """Get the modified Y axis."""
-        return self.y_modifier * super().getY()
+        value = super().getY()
+        if abs(value) < self.deadzone:
+            return 0
+        print("DOG - {}".format(self.y_modifier))
+        return self.y_modifier * value
 
     def getZ(self):
         """Get the modified Z axis."""
-        return self.z_modifier * super().getZ()
+        value = super().getZ()
+        if abs(value) < self.deadzone:
+            return 0
+        return self.z_modifier * value
