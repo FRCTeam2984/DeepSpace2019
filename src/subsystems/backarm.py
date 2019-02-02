92fdfae1f5b198048ea00e5f228520bf5dd06da2
ruwix <grantpauker@gmail.com>
2019-02-01 16:33:03 -0800

    Fix arm PIDF

--- src/subsystems/backarm.py
+++ src/subsystems/backarm.py
@@ -7,19 +7,20 @@ from utils import singleton, units, talonsrx, pidf
 
 
 class BackArm(Subsystem, metaclass=singleton.Singleton):
-    """The Drive subsystem controls the drive motors
-    and encoders."""
+    """The back arm subsystem controls the back arm motors and encoders."""
 
     def __init__(self):
         super().__init__()
 
     def init(self):
-        """Initialize the drive motors. This is not in the constructor to make the calling explicit in the robotInit to the robot simulator."""
+        """Initialize the back arm motors. This is not in the constructor to make the calling explicit in the robotInit to the robot simulator."""
         self.s_motor = talonsrx.TalonSRX(Constants.BS_MOTOR_ID)
         self.m_motor = talonsrx.TalonSRX(Constants.BM_MOTOR_ID)
         self.s_motor.initialize(inverted=False, encoder=False)
-        self.m_motor.initialize(inverted=False, encoder=True)
+        self.m_motor.initialize(inverted=False, encoder=False)
         self.s_motor.follow(self.m_motor)
+        self.m_motor.setPositionPIDF(
+            Constants.BACK_ARM_KP, Constants.BACK_ARM_KI, Constants.BACK_ARM_KD, Constants.BACK_ARM_KF)
 
     def zeroSensors(self):
         """Set the encoder positions to 0."""
@@ -29,10 +30,9 @@ class BackArm(Subsystem, metaclass=singleton.Singleton):
         pass
 
     def getAngle(self):
+        """Get the angle of the arm in degrees."""
         self.m_motor.getPosition()
 
-
-    def setPercentOutput(self, b_signal, f_signal):
-        """Set the percent output of the left and right motors."""
-        self.m_motor.setPercentOutput(b_signal)
-        self.m_motor.setPercentOutput(f_signal)
+    def setAngle(self, angle):
+        """Set the angle of the arm in degrees."""
+        self.m_motor.setPositionSetpoint(angle)
