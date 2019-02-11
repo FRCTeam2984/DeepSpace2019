a4fcb51fc34d6f5717f2bfadb486cac76658132c
ruwix <grantpauker@gmail.com>
2019-02-11 11:32:16 -0800

    Add throttle axis and joystick button

--- src/utils/joystick.py
+++ src/utils/joystick.py
@@ -6,11 +6,12 @@ class Joystick(joystick.Joystick):
     """A wraper for the wpilib joystick library that allows you
        to easily modify x, y, and z axis values."""
 
-    def __init__(self, port, x_modifier, y_modifier, z_modifier, deadzone=0.05):
+    def __init__(self, port, x_modifier, y_modifier, z_modifier, t_modifier, deadzone=0.05):
         super().__init__(port)
         self.x_modifier = x_modifier
         self.y_modifier = y_modifier
         self.z_modifier = z_modifier
+        self.t_modifier = t_modifier
         self.deadzone = deadzone
 
     def getX(self):
@@ -33,3 +34,14 @@ class Joystick(joystick.Joystick):
         if abs(value) < self.deadzone:
             return 0
         return self.z_modifier * value
+
+    def getThrottle(self):
+        """Get the modified throttle axis."""
+        value = super().getThrottle()
+        if abs(value) < self.deadzone:
+            return 0
+        return self.t_modifier * value
+
+    def getJoystickButton(self, button_num):
+        """Get a joystick button."""
+        return joystickbutton.JoystickButton(self, button_num)
