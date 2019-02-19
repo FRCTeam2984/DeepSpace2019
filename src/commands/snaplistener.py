b6fb4a5f83512dc01c9d134244df09c50b5fbebe
Cameron Reikes <cameronreikes@gmail.com>
2019-02-18 17:20:27 -0800

    Snap to odd angles for match

--- src/commands/snaplistener.py
+++ src/commands/snaplistener.py
@@ -8,6 +8,8 @@ from subsystems import drive
 from utils import angles
 
 class SnapListener(Command):
+    POV_ARRAY = [0, 28.75, 90, 151.25, -179.9, -151.25, -90, -28.75]
+
     def __init__(self, pov_port):
         super().__init__()
         self.drive = drive.Drive()
@@ -21,6 +23,9 @@ class SnapListener(Command):
         #logging.debug("drive current command: {}".format(self.drive.currentCommand))
         if pov != -1 and not isinstance(self.drive.currentCommand,turntoangle.TurnToAngle):
             pov = angles.positiveAngleToMixedAngle(pov)
-            Dash.putNumber("Snap Turning Target", pov)
+            pov /= 45
+            to_turn_to = SnapListener.POV_ARRAY[pov]
+            to_turn_to = int(to_turn_to)
+            Dash.putNumber("Snap Turning Target", to_turn_to)
             logging.debug("Starting snap to angle")
-            turntoangle.TurnToAngle(pov).start()
+            turntoangle.TurnToAngle(to_turn_to).start()
