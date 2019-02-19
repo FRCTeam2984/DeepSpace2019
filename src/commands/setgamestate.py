5cdb79f07945ec4c04a4d5a073b720334eb991a9
ruwix <grantpauker@gmail.com>
2019-02-18 17:16:03 -0800

    Move command adding to init

--- src/commands/setgamestate.py
+++ src/commands/setgamestate.py
@@ -4,17 +4,20 @@ from subsystems import intake
 from utils.gamestate import GameState
 from commands import setshortarm, setlongarm
 from constants import Constants
+from wpilib import SmartDashboard as Dash
+import logging
 
 
 class SetGameState(CommandGroup):
     def __init__(self, state):
         super().__init__()
         self.state = state
+        posture = Constants.GAME_STATES[self.state.value]
+        self.addSequential(setshortarm.SetShortArm(posture[0]))
+        self.addSequential(setlongarm.SetLongArm(posture[2]))
 
     def initialize(self):
-        posture = Constants.GAME_STATES[self.state]
-        self.addParallel(setshortarm.SetShortArm(posture[0]))
-        self.addParallel(setlongarm.SetLongArm(posture[2]))
+        pass
 
     def end(self):
         pass
