26eadc8b6c9a5169b7cdf20d3a99d2131f71f5e7
ruwix <grantpauker@gmail.com>
2019-02-18 17:15:18 -0800

    Make instant command

--- src/commands/rollclimbroller.py
+++ src/commands/rollclimbroller.py
@@ -1,29 +1,17 @@
-from wpilib.command import Command
+from wpilib.command import InstantCommand
 
 from constants import Constants
 from subsystems import climbroller
 import wpilib
+import logging
 
 
-class RollClimbRoller(Command):
+class RollClimbRoller(InstantCommand):
     def __init__(self, speed):
         super().__init__()
         self.climbroller = climbroller.ClimbRoller()
         self.requires(self.climbroller)
         self.speed = speed
-        self.setInterruptible(True)
 
     def initialize(self):
-        pass
-
-    def execute(self):
         self.climbroller.roll(self.speed)
-
-    def isFinished(self):
-        return False
-
-    def interrupted(self):
-        self.end()
-
-    def end(self):
-        self.climbroller.stop()
